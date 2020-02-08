13eb553fac72013857b4a8edd2f238e67484d0b2
Takuro Niitsuma <bakednt@gmail.com>
2020-02-08 20:41:05 +0900

    tokenのidfによる重み付き平均にしてみる

--- CaBE/language_model_encoder.py
+++ CaBE/language_model_encoder.py
@@ -1,9 +1,12 @@
 import os
+from collections import defaultdict
+
+import numpy as np
 import torch
 from transformers import BertTokenizer, BertModel
 
 SAVED_MODEL_PATH = '/tmp/MODEL'
-PRETRAINED_BERT_NAME = 'bert-large-uncased'
+PRETRAINED_BERT_NAME = 'bert-base-uncased'
 
 
 class BertEncoder:
@@ -24,15 +27,41 @@ class BertEncoder:
 
     def encode(self, phrases, num_layers=12):
         encoded_phrases = []
+
+        token_ids_per_phrase = []
         for phrase in phrases:
-            input_ids = torch.tensor([self.tokenizer.encode(phrase, max_length=7, pad_to_max_length=True)])
+            token_ids_per_phrase.append(
+                torch.tensor(self.tokenizer.encode(phrase,
+                                                   max_length=7,
+                                                   pad_to_max_length=True),
+                             dtype=torch.long))
+
+        token_idfs = calc_token_idfs(token_ids_per_phrase)
+
+        for token_ids in token_ids_per_phrase:
             with torch.no_grad():
-                _, _, hidden_states = self.model(input_ids)
+                _, _, hidden_states = self.model(token_ids.unsqueeze(0))
                 encoded_tokens = hidden_states[num_layers].squeeze()
-                encoded_phrase = torch.mean(encoded_tokens, 1)
+                weights_by_idf = [token_idfs[int(tid)]+1 for tid in token_ids]
+                weighted_tensor = torch.tensor(weights_by_idf, dtype=torch.long).view(-1, 1)
+
+                sum_of_weighted_tokens = torch.sum(encoded_tokens * weighted_tensor, axis=1)
+                encoded_phrase = sum_of_weighted_tokens / torch.sum(weighted_tensor)
             encoded_phrases.append(encoded_phrase)
 
         return torch.stack(encoded_phrases, axis=0)
+
+
+def calc_token_idfs(token_ids_per_phrase):
+    num_phrases = len(token_ids_per_phrase)
+    token_dfs = defaultdict(int)
+
+    for token_ids in token_ids_per_phrase:
+        for token_id in token_ids.unique():
+            token_dfs[int(token_id)] += 1
+
+    token_idfs = {k: np.log(num_phrases / v) for k, v in token_dfs.items()}
+    return token_idfs
 
 
 class ElmoEncoder:
