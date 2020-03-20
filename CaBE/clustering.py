261317bab486575e6084571f2c95016b5beb73f6
Takuro Niitsuma <bakednt@gmail.com>
2020-03-20 18:06:33 +0900

    OPIECからデータセット作るやつ書いた

--- CaBE/clustering.py
+++ CaBE/clustering.py
@@ -10,15 +10,22 @@ class HAC:
         self.similarity = similarity
         self.linkage = linkage
         self.n_layer = n_layer
-        self.clustering = AgglomerativeClustering(
-            distance_threshold=self.threshold,
-            n_clusters=None,
-            affinity=self.similarity,
-            linkage=self.linkage)
 
     def run(self, elements):
-        assigned_clusters = self.clustering.fit_predict(elements)
+        if self.similarity == 'mahalanobis':
+            V = empirical_covariance(elements)
+            affinity = lambda X: pairwise_distances(X, metric='mahalanobis', V=V)
+        else:
+            affinity = self.similarity
+        assigned_clusters = self.clustering(affinity).fit_predict(elements)
         return hlp.transform_clusters(assigned_clusters)
+
+    def clustering(self, affinity):
+        return AgglomerativeClustering(
+            distance_threshold=self.threshold,
+            n_clusters=None,
+            affinity=affinity,
+            linkage=self.linkage)
 
     @property
     def name(self):
